b9e1949c9a3e82eec2ffed612f2983bb58a4b563
Adrian Ochoa <adrianfeloh1@gmail.com>
2023-05-19 19:45:35 -0500

    ejercicio con useLayoutEffect

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,9 +1,27 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
 export const Quote = ({ data }) => {
+  const pRef = useRef();
+
+  const [boxSize, setbBoxSize] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect(() => {
     
+    const {width, height } = pRef.current.getBoundingClientRect()
+    setbBoxSize({width, height})
+  }, [data.quote]);
+
   return (
-    <blockquote className="blockquote text-end">
-      <p className="mb-3"> {data[0].quote} </p>
-      <footer className="blockquote-footer">{data[0].author} </footer>
-    </blockquote>
+    <>
+      <blockquote className="blockquote text-end" style={{ display: "flex" }}>
+        <p ref={pRef} className="mb-3">
+          {" "}
+          {data[0].quote}{" "}
+        </p>
+        <footer className="blockquote-footer">{data[0].author} </footer>
+      </blockquote>
+
+      <code>{ JSON.stringify(boxSize) }</code>
+    </>
   );
 };
