d2cc38db3f832a40486a844471b53910c96129ef
Adrian Ochoa <adrianfeloh1@gmail.com>
2023-05-24 20:47:50 -0500

    Finalizando con useReducer

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,48 +1,34 @@
-import { useReducer } from "react";
-import { todoReducer } from "./todoReducer";
+
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./Todolist";
+import { useTodos } from "../hooks";
 
-const initialState = [
-  {
-    id: new Date().getTime(),
-    description: "Recolectar piedra del alma",
-    done: false,
-  },
-  {
-    id: new Date().getTime() * 3,
-    description: "Recolectar piedra Verde Esperanza",
-    done: false,
-  },
-];
 
-export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState);
 
-  const handleNewTodo = (todo) => {
-    console.log({ todo })
-  }
+export const TodoApp = () => {  
+
+  const {todos, todosCount, pendingsTodosCount, handleDeleteTodo, handleNewTodo, handleToggleTodo} = useTodos()
 
   return (
     <>
       <h1>
-        TodoApp: 10, <small>pendientes: 2</small>
+        TodoApp: {todosCount}, <small>pendientes: {pendingsTodosCount}</small>
       </h1>
       <hr />
 
       <div className="row">
         <div className="col-7">
-
-          <TodoList todos = {todos} />
-
-
+          <TodoList
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
         </div>
         <div className="col-5">
-
           <h4>Agregar TODO</h4>
           <hr />
-          
-          <TodoAdd onNewTodo={handleNewTodo}/>
+
+          <TodoAdd onNewTodo={handleNewTodo} />
         </div>
       </div>
     </>
--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -2,10 +2,25 @@
 export const todoReducer = (initialState = [], action) => {
 
     switch (action.type) {
-        case 'ABC':
+        case '[TODO] Add Todo':
+            return [ ...initialState, action.payload ];
+
+        case '[TODO] Remove Todo':
+            return initialState.filter( todo => todo.id !== action.payload );  
             
-            throw new Error ('Action.type = ABC no está implementada');
-    
+        case '[TODO] Toggle Todo':
+            return initialState.map ( todo => {
+
+                if (todo.id === action.payload){
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+
+                return todo;
+            })
+               
         default:
             return initialState;
     }
